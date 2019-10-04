6cba5c5e8f0f1100dd51c1fe712e7af848894c6b
Charles-Eugene Loubao <charlesloubao95@gmail.com>
2019-10-04 09:34:34 -0400

    Fixed output of webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const path = require('path')
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'public'),
+        path: path.resolve(__dirname, 'dist'),
     },
     devServer: {
         contentBase: path.join(__dirname, "public"),
@@ -24,5 +25,10 @@ module.exports = {
                 ],
             }
         ]
-    }
+    },
+    plugins: [
+        new CopyPlugin([
+            { from: 'public', to: '' }
+        ])
+    ]
 }
